5de288603dfc473a0b98c58c3f39a28ef02e049e
jeremyhughes <jeremy.l.hughes@gmail.com>
2024-08-18 23:47:59 +1000

    more work

--- myscripts.js
+++ myscripts.js
@@ -5,6 +5,9 @@
 //        $(".small-image").removeClass('active');
 //       $("#show_image_popup").hide();
 //     })
+
+const { artData } = require("./artData");
+
   
   
 //     $(".small-image").click(function(){
@@ -71,15 +74,15 @@
       id_of_painting = src_val.split('/')[src_val.split('/').length-1]
       
       //setting the correct data
-
       var artistString = '<em>'+artData[id_of_painting]["artist"]+'</em>'+' ('+artData[id_of_painting]['nationality']+', '+artData[id_of_painting]['yob']+'-'+artData[id_of_painting]['yod']+')';
       var titleString = '<i><b>'+artData[id_of_painting]['title']+',</i></b>'+' '+artData[id_of_painting]['paintingDate'];
       var mediumString = artData[id_of_painting]['medium'];
+      var extraLinkString = 'For more information please visit <a href='+artData[id_of_painting]["extraLink"]+'> this site</a>.';
       document.getElementById("title").innerHTML = titleString;
       document.getElementById("artist").innerHTML = artistString;
       document.getElementById('medium').innerHTML = mediumString;
       document.getElementById('comment').innerHTML = artData[id_of_painting]['comment']
-      document.getElementById('extraLink').innerHTML = artData[id_of_painting]['extraLink'] 
+      document.getElementById('extraLink').innerHTML = extraLinkString
       showModal();
     });
   }
@@ -104,27 +107,3 @@
   }
 
 
-  //writing in all the artData, there must be a cleaner way to do this. JS is so janky
-
-  let artData = {
-    "0101_Pity_WilliamBlake.jpg":{
-      "title":"Pity",
-      "artist":"William Blake",
-      "nationality":"English",
-      "pob":'Soho',
-      'yob':'1757',
-      'yod':'1827',
-      'pod':'London',
-      'paintingDate':'c.1795',
-      'medium':'Colour print, finished in ink and water colour, on paper.',
-      'style':'Romantasicim',
-      'genre':'',
-      'period':'',
-      'comment':"This artwork is a literal illustration of a double simile from <i>Macbeth</i>, found in the lines: \nAnd pity, like a naked new-born babe,\nStriding the blast, or heaven's cherubim, hors'd\nUpon the sightless couriers of the air.\n- Macbeth (1.7.21–23)[3]",
-      'extraLink':'https://www.metmuseum.org/art/collection/search/347416'
-    },
-    "0102_TheBattleOfSanRomano_PaoloUccello.jpg":{
-      "title":"The Battle of San Romano",
-      "artist":"Paolo Uccello"
-    }
-  }
